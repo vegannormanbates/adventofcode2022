a45ed3aca4253a12e277196b2f49f8145f02af10
vegannormanbates <119760413+vegannormanbates@users.noreply.github.com>
2022-12-05 13:01:46 -0600

    Day 5

---
+++ Day 5/day5.pl
@@ -0,0 +1,84 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+use POSIX;
+
+open (file,'<',"input_data");
+
+my $numStacks=0;
+my @stacks;
+
+while(<file>)
+{
+    chomp $_;
+    if (index($_,"move")== -1&& length($_) > 0)
+    {
+        if(index($_,"[")==-1){next;}
+
+        my $rowLength = length($_);
+        my $row = $_;
+        my $counter = 0;
+        my $itemLocation=0;
+        while ($counter < $rowLength)
+        {
+            $itemLocation = index($row,"[",($itemLocation));
+    
+            if($itemLocation==0)
+            {
+                
+                push(@{$stacks[0]},substr($row,1,1));
+            }
+            if($itemLocation>0)
+            {
+                my $numSpaces = floor($itemLocation/3);
+                my $value = $itemLocation - $numSpaces;
+                my $column = ceil($value/3);
+                push(@{$stacks[$column]},substr($row,($itemLocation+1),1));
+            }
+            $counter= $itemLocation;
+            if($counter == -1){last;}
+            $itemLocation++;
+        }
+    }
+    elsif (length($_) == 0)
+    {
+        my $localCount=0;
+        while ($localCount < @stacks)
+        {
+            @{$stacks[$localCount]}= reverse(@{$stacks[$localCount]});
+            $localCount++;
+        }
+    }
+    else
+    {
+        my @command = split(" ",$_);
+        my $numMove = $command[1];
+        my $source = $command[3]-1;
+        my $dest = $command[5]-1;
+        my @crate;
+        my $count = 0;
+        while ($count < $numMove)
+        {
+            
+            my $temp = pop(@{$stacks[$source]});
+            push(@crate,$temp);
+            $count++;
+        }
+        $count = 0;
+        while ($count < $numMove)
+        {
+            my $temp = pop(@crate);
+            push(@{$stacks[($dest)]},$temp);
+            $count++;
+        }
+    }
+}
+#print Dumper @stacks;
+
+my $localCount=0;
+while ($localCount < @stacks)
+{
+    print pop(@{$stacks[$localCount]});
+    $localCount++;
+}
